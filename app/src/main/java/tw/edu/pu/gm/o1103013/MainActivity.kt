f789edfb533d0bf8ea1f885b230d2099b77cfed5
ivvin <o1103013@gm.pu.edu.tw>
2023-03-22 14:22:36 +0800

    v.o(徐维骏)

--- app/src/main/java/tw/edu/pu/gm/o1103013/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/o1103013/MainActivity.kt
@@ -2,10 +2,22 @@ package tw.edu.pu.gm.o1103013
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txv : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv = findViewById(R.id.txv)
+        txv.text = "Brook"
+        txv.setOnClickListener({
+            if (txv.text == "yohohoho"){
+                txv.text = "Golden Panties"
+            }else{
+                txv.text = "yohohoho"
+            }
+        })
     }
 }
